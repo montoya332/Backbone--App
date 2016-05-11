767ec1a6e2f14993618c8aa406cf6b40b2ca1c0e
Arturo Montoya <montoya332@live.com>
2016-05-11 12:05:58 -0700

    Added: Marionette

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,16 +5,33 @@ require.config({
         jquery: '../bower_components/jquery/dist/jquery',
         underscore: '../bower_components/underscore/underscore',
         backbone: '../bower_components/backbone/backbone',
+        'backbone.picky': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
+        marionette: '../bower_components/backbone.marionette/lib/backbone.marionette',
         materialize: '../bower_components/Materialize/dist/js/materialize',
         handlebars: '../bower_components/handlebars/handlebars',
         text: '../bower_components/text/text'
     },
-    shim: {},
+    shim: {
+        jquery: {
+            exports: '$'
+        },
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        marionette: {
+            deps: ['backbone'],
+            exports: 'Marionette'
+        }
+    },
     waitSeconds: 200
 });
 
-require(['jquery', 'underscore', 'backbone', 'views/RestaurantView', 'models/Restaurant', 'collections/RestaurantCollection','routes/Restaurant_Routes'],
-    function($, _, Backbone, RestaurantView, RestaurantModel, RestaurantCollection,MainRouter) {
+require(['jquery', 'underscore', 'backbone', 'views/RestaurantView', 'models/Restaurant', 'collections/RestaurantCollection', 'routes/Restaurant_Routes'],
+    function($, _, Backbone, RestaurantView, RestaurantModel, RestaurantCollection, MainRouter) {
 
         'use strict';
         console.log('webApp', webApp);
